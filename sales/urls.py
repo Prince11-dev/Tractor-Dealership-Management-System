95524549bfaf113eddf5ebfff2dcc10b565509a6
Prince11-dev <giriprince711@gmail.com>
2026-05-25 22:54:44 +0530

    Completed sales CRUD module

--- sales/urls.py
+++ sales/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
-from .views import sales_list, add_sale
+from .views import (
+    sales_list,
+    add_sale,
+    update_sale,
+    delete_sale
+)
 
 urlpatterns = [
 
     path('', sales_list, name='sales'),
     path('add/', add_sale, name='add_sale'),
+    path('update/<int:id>/', update_sale, name='update_sale'),
+    path('delete/<int:id>/', delete_sale, name='delete_sale'),
 
 ]
--- sales/views.py
+++ sales/views.py
@@ -10,7 +10,7 @@ def sales_list(request):
         'sales': sales
     }
 
-    return render(request, 'sales.html', context)
+    return render(request,'sales.html',context)
 
 
 def add_sale(request):
@@ -18,7 +18,7 @@ def add_sale(request):
     customers = Customer.objects.all()
     tractors = Tractor.objects.all()
 
-    if request.method == 'POST':
+    if request.method=="POST":
 
         customer = Customer.objects.get(
             id=request.POST['customer']
@@ -28,8 +28,8 @@ def add_sale(request):
             id=request.POST['tractor']
         )
 
-        sale_date = request.POST['sale_date']
-        amount = request.POST['amount']
+        sale_date=request.POST['sale_date']
+        amount=request.POST['amount']
 
         Sale.objects.create(
             customer=customer,
@@ -40,11 +40,51 @@ def add_sale(request):
 
         return redirect('sales')
 
-    context = {
-        'customers': customers,
-        'tractors': tractors
+    context={
+        'customers':customers,
+        'tractors':tractors
     }
 
-    return render(request, 'add_sale.html', context)
+    return render(request,'add_sale.html',context)
 
+
+def update_sale(request,id):
+
+    sale = Sale.objects.get(id=id)
+
+    customers = Customer.objects.all()
+    tractors = Tractor.objects.all()
+
+    if request.method=="POST":
+
+        sale.customer=Customer.objects.get(
+            id=request.POST['customer']
+        )
+
+        sale.tractor=Tractor.objects.get(
+            id=request.POST['tractor']
+        )
+
+        sale.sale_date=request.POST['sale_date']
+        sale.amount=request.POST['amount']
+
+        sale.save()
+
+        return redirect('sales')
+
+    context={
+        'sale':sale,
+        'customers':customers,
+        'tractors':tractors
+    }
+
+    return render(request,'update_sale.html',context)
+
+
+def delete_sale(request,id):
+
+    sale=Sale.objects.get(id=id)
+    sale.delete()
+
+    return redirect('sales')
 # Create your views here.
